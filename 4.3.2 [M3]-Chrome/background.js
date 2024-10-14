ca56fceaa16480ae2940ba1b6318165ca5c3ef2a
hemucode <69314427+hemucode@users.noreply.github.com>
2024-10-14 23:08:00 +0530

    4.3.2

---
+++ 4.3.2 [M3]-Chrome/background.js
@@ -0,0 +1,409 @@
+/* © Copyright 2024, Hemanta Gayen, All rights reserved. */
+/*!
+ *  NonStop YouTube™ by @codehemu -https://github.com/hemucode/nonstop-youtube/
+ *  License - https://github.com/hemucode/nonstop-youtube/license ( CSS: MIT License)
+ */
+
+/**
+ * Here are the IDs and classes of all YouTube advertisement elements.
+ * adBlockingSelectorsFallback Always changing. Here are the IDs and classes of 
+ * all types of banner ads and other ads that can be easily targeted and remove.
+ * rights only @codehemu.
+ */
+
+const adBlockingSelectorsFallback = [
+  'ytd-promoted-video-renderer',
+  'ytd-movie-offer-module-renderer',
+  'ytd-promoted-sparkles-web-renderer',
+  'ytd-promoted-sparkles-text-search-renderer',
+  'ytd-player-legacy-desktop-watch-ads-renderer',
+  '#player-ads',
+  '#search-pva',
+  '#premium-yva',
+  '#masthead-ad',
+  '#feedmodule-PRO',
+  '#video-masthead',
+  '#watch-buy-urls',
+  '#sub-frame-error',
+  '#main-frame-error',
+  '#watch7-sidebar-ads',
+  '#feed-pyv-container',
+  '#shelf-pyv-container',
+  '#watch-branded-actions',
+  '#watch-channel-brand-div',
+  '#homepage-chrome-side-promo',
+  '#watch-channel-brand-div-text',
+  '.iv-promo',
+  '.video-ads',
+  '.promoted-videos',
+  '.ytp-ad-progress',
+  '.ytp-ad-progress-list',
+  '.searchView.list-view',
+  '.html5-ad-progress-list',
+  '.watch-extra-info-right',
+  '.watch-extra-info-column',
+  '.lohp-pyv-shelf-container',
+  '.ytd-merch-shelf-renderer',
+  '.carousel-offer-url-container',
+  '.youtubeSuperLeaderBoardAdHolder',
+  '.youtubeSuperLeaderOverallAdArea',
+  '.ytd-movie-offer-module-renderer',
+  '.ytd-action-companion-ad-renderer',
+  'iframe[id^=ad_]',
+  'div[class*="-ad-v"]',
+  'div[class*="-ads-"]',
+  'a[href*="/adwords/"]',
+  'a[href*="doubleclick.net"]',
+  'iframe[src*="doubleclick.net"]',
+  'a[href^="http://www.youtube.com/cthru?"]',
+  'a[href^="https://www.youtube.com/cthru?"]',
+  'a[onclick*="\\"ping_url\\":\\"http://www.google.com/aclk?"]',
+  '.ad-showing > .html5-video-container',
+  '.ytd-player-legacy-desktop-watch-ads-renderer',
+  '.ytd-rich-item-renderer > ytd-ad-slot-renderer',
+  'ytd-engagement-panel-section-list-renderer[target-id="engagement-panel-ads"]',
+  '#contents > ytd-rich-item-renderer:has(> #content > ytd-ad-slot-renderer)',
+  '.ad-container',
+  'ytd-display-ad-renderer',
+  '.ytd-carousel-ad-renderer',
+  'ytd-compact-promoted-video-renderer',
+  '.ytd-promoted-sparkles-text-search-renderer',
+  '.masthead-ad-control',
+  '#ad_creative_3',
+  '#footer-ads',
+  '.ad-div',
+  '.ytd-mealbar-promo-renderer',
+  '.sparkles-light-cta',
+  '.badge-style-type-ad',
+  '.GoogleActiveViewElement',
+  '.ytd-compact-promoted-video-renderer',
+  '.ytd-companion-slot-renderer',
+  '.ytd-video-masthead-ad-v3-renderer',
+  '[layout*="display-ad-"]',
+  '#merch-shelf',
+  '#show-ad',
+  '.ytd-in-feed-ad-layout-renderer',
+  '.ytp-ad-image-overlay',
+  '.ytp-ad-text-overlay',
+  '.ytd-ad-slot-renderer',
+  '.companion',
+  'ytd-compact-promoted-item-renderer',
+  '.html5-video-player.ad-showing video'
+].join(",");
+
+const regexRulesFallback = [  
+  "googleads.g.doubleclick.net",
+  "=adunit&",
+  "/pubads.",
+  "/pubads_",
+  "/api/ads/",
+  "/googleads_",
+  "innovid.com",
+  "/pagead/lvz?",
+  "/pagead/gen_",
+  "doubleclick.com",
+  "google.com/pagead/",
+  "youtube.com/pagead/",
+  "googlesyndication.com",
+  "www.youtube.com/get_midroll_"
+];
+/**
+ * It is used to determine whether a start will run or stop.
+ * Where I mean an install will continue.
+ * rights only @codehemu.
+ */
+chrome.runtime.onStartup.addListener(async () => {
+  var codehemu = new Promise(function(resolve, reject){
+        chrome.storage.sync.get({"enabled": true}, function(options){
+            resolve(options.enabled);
+      })
+  });
+
+  const enabled = await codehemu;
+
+  if (enabled) {await enable()} else {await disable()}
+
+});
+
+
+/**
+ * Do not use google API without permission from the developer.
+ * This key has rights only @codehemu. You may not use it at all without CodeHemu permission.
+ * https://script.google.com/macros/s/++/exec
+ */
+const API_KEY = "AKfycbzh4Bsea_YgsPn9MUOYzbSDY6O0g-uiqIGW62-msKW9kVxOJ12rkhOi9rDExSCErUA";
+
+/**
+ * If the user uninstalls the app then he will have a new url or windows open.
+ * rights only @codehemu.
+ */
+var uninstallUrl = `${chrome.runtime.getManifest().homepage_url}#uninstall`;
+chrome.runtime.setUninstallURL(uninstallUrl);
+
+
+/**
+ * When the app is installed, the date the app was installed and other 
+ * information will be stored in the browser.
+ * rights only @codehemu.
+ */
+
+chrome.runtime.onInstalled.addListener(async (details) => {
+  switch (details.reason) {
+    case chrome.runtime.OnInstalledReason.INSTALL:
+      chrome.storage.sync.set({
+        installDate: Date.now(),
+        installVersion: chrome.runtime.getManifest().version,
+        regexRules: regexRulesFallback,
+        adBlockingSelectors: adBlockingSelectorsFallback,
+        isPlayerContainer: ".html5-video-player.ad-showing video",
+        isSkipButtonContainer: ".ytp-ad-skip-button",
+        isSkipAdsEnabled: true,
+        lessCurrentTime: 1.5,
+        lessDurationTime: 10,
+        minusDurationTime: 3,
+        isSpeedAdsEnabled: false,
+        videoSpeed: 2,
+        isRecommendEnabled: false,
+        isRateUsPopupEnabled: true,
+        updateVersionChrome: chrome.runtime.getManifest().version,
+        isMessage: "",
+        isMessageURL: "",
+        nonstop: true,
+        comments: true,
+        shorts: false
+      });
+      chrome.tabs.create({ url: chrome.runtime.getManifest().homepage_url });
+
+    case chrome.runtime.OnInstalledReason.UPDATE:
+      chrome.storage.sync.set({
+        updateDate: Date.now(),
+      });
+      
+  }
+});
+
+
+/**
+ * If you turn it on, it will run.
+ * rights only @codehemu.
+ */
+
+chrome.storage.onChanged.addListener(async (changes, namespace) => {
+  if (namespace !== "sync") return;
+  if (changes.enabled) {
+    if (changes.enabled.newValue) {
+      await enable();
+    } else {
+      await disable();
+    }
+  }else if(changes.nonstop || changes.comments || changes.shorts){
+    await reloadAffectedTab();
+  }
+
+});
+
+
+/**
+ * Used to reload youtube page it will only reload youtube page not reload other pages.
+ * rights only @codehemu.
+ * @returns reload youtube page.
+ */
+
+async function reloadAffectedTab() {
+  const [currentTab] = await chrome.tabs.query({
+    active: true,
+    url: "*://*.youtube.com/*",
+  });
+  const isTabAffected = Boolean(currentTab?.url);
+  if (isTabAffected) {
+    return chrome.tabs.reload();
+  }
+}
+
+
+
+/**
+ * chrome.declarativeNetRequest.updateDynamicRules() API works, I ended up with this solution.
+ * blockUrls is an array variable that holds all blocked websites/URLs.
+ * rights only @codehemu.
+ */
+
+async function UpdateIntercept() {
+    var promise = new Promise((resolve, reject)=>{
+          chrome.storage.sync.get({"enabled": true,"regexRules": regexRulesFallback}, (options)=>{
+              resolve(options);
+        })
+    });
+    const options = await promise;
+
+    if (options.enabled) {
+        const allResourceTypes = Object.values(chrome.declarativeNetRequest.ResourceType);
+
+        const newRules = [];
+        options.regexRules.forEach((domain, index) => {
+          newRules.push({
+            "id": index + 1,
+            "priority": 1,
+            "action": { "type": "block" },
+            "condition": {"domains": ['youtube.com'], "urlFilter": domain, "resourceTypes": allResourceTypes }
+          });
+        });
+
+        chrome.declarativeNetRequest.getDynamicRules(previousRules => {
+          const previousRuleIds = previousRules.map(rule => rule.id);
+          chrome.declarativeNetRequest.updateDynamicRules({
+            removeRuleIds: previousRuleIds,
+            addRules: newRules
+          });
+        });
+    }else{
+        chrome.declarativeNetRequest.getDynamicRules(previousRules => {
+          const previousRuleIds = previousRules.map(rule => rule.id);
+          chrome.declarativeNetRequest.updateDynamicRules({
+            removeRuleIds: previousRuleIds
+          });
+        });
+    }
+}
+
+
+/**
+ * By looking at the name of the function, you can understand what it actually is function, 
+ * it is the function of being active.
+ * rights only @codehemu.
+ */
+
+async function enable() {
+  await UpdateIntercept();
+  await reloadAffectedTab();
+}
+
+/**
+ * As with the function above, it's inverse.
+ * This is the closing function.
+ * rights only @codehemu.
+ */
+async function disable() {
+  await UpdateIntercept();
+  await reloadAffectedTab();
+}
+
+/**
+ * It is used to store all kinds of AdBlocking Selectors IDs and class names.
+ * and others store
+ * rights only @codehemu.
+ */
+
+/**
+ * It is used to store all kinds of AdBlocking Selectors IDs and class names.
+ * and others store
+ * rights only @codehemu.
+ */
+
+const setAdBlockingSelectors = async (
+  regexRules,
+  adBlockingSelectors, 
+  isPlayerContainer,
+  isSkipButtonContainer,
+  isSkipAdsEnabled,
+  lessCurrentTime,
+  lessDurationTime,
+  minusDurationTime,
+  isSpeedAdsEnabled,
+  videoSpeed,
+  isRecommendEnabled,
+  isRateUsPopupEnabled,
+  updateVersionChrome,
+  isMessage,
+  isMessageURL) => {
+  await chrome.storage.sync.set({ 
+    regexRules,
+    adBlockingSelectors, 
+    isPlayerContainer,
+    isSkipButtonContainer,
+    isSkipAdsEnabled,
+    lessCurrentTime,
+    lessDurationTime,
+    minusDurationTime,
+    isSpeedAdsEnabled,
+    videoSpeed,
+    isRecommendEnabled,
+    isRateUsPopupEnabled,
+    updateVersionChrome,
+    isMessage,
+    isMessageURL
+  });
+};
+
+
+/**
+ * It is used externally to update all the data it requests to download from the URL.
+ * rights only @codehemu.
+ */
+
+const updateJson = async () => {
+  url = `https://script.google.com/macros/s/${API_KEY}/exec?v=nonstop`;
+  await fetch(url)
+  .then((response) => response.json())
+  .then((json) => {
+    //console.log(json);
+     const {
+        regexRules,
+        adBlockingSelectors,
+        isPlayerContainer,
+        isSkipButtonContainer,
+        isSkipAdsEnabled,
+        lessCurrentTime,
+        lessDurationTime,
+        minusDurationTime,
+        isSpeedAdsEnabled,
+        videoSpeed,
+        isRecommendEnabled,
+        isRateUsPopupEnabled,
+        updateVersionChrome,
+        isMessage,
+        isMessageURL
+     } = json;
+     // console.log(json);
+     // console.log(updateVersionChrome);
+
+     setAdBlockingSelectors(regexRules,
+        adBlockingSelectors.join(","), 
+        isPlayerContainer,
+        isSkipButtonContainer,
+        isSkipAdsEnabled,
+        lessCurrentTime,
+        lessDurationTime,
+        minusDurationTime,
+        isSpeedAdsEnabled,
+        videoSpeed,
+        isRecommendEnabled,
+        isRateUsPopupEnabled,
+        updateVersionChrome,
+        isMessage,
+        isMessageURL);
+  })
+  .catch((e) => {
+    console.error(e);
+   });
+};
+
+const init = async () => {
+  updateJson();
+	chrome.runtime.onMessage.addListener(
+		({ action, href, message }, { tab }, sendResponse) => {
+	    if (action === "PAGE_READY") {
+		    const response = {
+		      adBlockSelectors: adBlockingSelectorsFallback
+		    };
+		    updateJson();
+		    sendResponse(response);
+	  } 
+	});
+
+};init();
+
+
+
+
+
---
+++ 4.3.2 [M3]-Chrome/codehemu-content-script.js
@@ -0,0 +1,531 @@
+/* © Copyright 2024, Hemanta Gayen, All rights reserved. */
+/*!
+ *  NonStop YouTube™ by @codehemu -https://github.com/hemucode/nonstop-youtube/
+ *  License - https://github.com/hemucode/nonstop-youtube/license ( CSS: MIT License)
+ */
+
+
+const WEBSTORE = `https://chrome.google.com/webstore/detail/${chrome.runtime.id}`;
+const WEBSTORE_REVIEW = `${WEBSTORE}/reviews`;
+
+
+const isIframe = () => {
+  try {
+    return window.self !== window.top;
+  } catch (e) {
+    return true;
+  }
+};
+
+const closeConfirmButton = ()=> {
+  try{
+    return new Promise((resolve) => {
+      const confirmButton = document.getElementById("confirm-button");
+      if (confirmButton) {
+        confirmButton.click();
+        resolve(true);
+      }
+      resolve(false);
+    });
+  } catch (e) {
+    return false;
+  }
+}
+
+const removeStillWatchingPopup = ()=> {
+  try{
+    return new Promise((resolve) => {
+      [
+        "Still watching? Video will pause soon",
+        "Bist du noch da? Die Wiedergabe wird bald unterbrochen.",
+        "Video paused. Continue watching?",
+        
+      ].forEach((text) => {
+        const areYouStillTherePopup = document.evaluate(
+          `//div[contains(text(),"${text}")]`,
+          document,
+          null,
+          XPathResult.FIRST_ORDERED_NODE_TYPE,
+          null
+        ).singleNodeValue;
+        if (areYouStillTherePopup) {
+          document.querySelectorAll("tp-yt-paper-toast").forEach((toast) => {
+            if (
+              toast.label &&
+              toast.label !== undefined &&
+              toast.label.includes(text)
+            ) {
+              toast.getElementsByTagName("button")[0].click();
+              resolve(true);
+            }
+          });
+        }
+      });
+
+      resolve(false);
+    });
+
+  }catch (e) {
+    return false;
+  }
+}
+
+class CosmeticFilter {
+  adBlockSelectors = "";
+
+  constructor(adBlockSelectors) {
+    this.adBlockSelectors = adBlockSelectors;
+  }
+
+  start() {
+    let headElement = document.head || document.getElementsByTagName("head")[0];
+    let bodyElement = document.body || document.getElementsByTagName("body")[0];
+
+    if (!headElement || !bodyElement) return void setTimeout((() => {
+      new CosmeticFilter(this.adBlockSelectors,this.removeVideoAdsScript).start();
+    }), 100);
+
+    const newCssContent = `
+    .yt-ext-hidden {
+      display: none;
+    }
+
+    #yt-ext-info-bar {
+      position: fixed;
+      bottom: 20px;
+      left: 20px;
+      height: auto;
+      border: 1px solid #ececec;
+      background: #ff0000;
+      z-index: 100000;
+      padding: 5px 12px;
+      color: white;
+      font-size: 14px;
+      border-radius: 4px;
+    }
+
+    .ad-container,
+    .ad-div,
+    .masthead-ad-control,
+    .video-ads,
+    .ytp-ad-progress-list,
+    #ad_creative_3,
+    #footer-ads,
+    #masthead-ad,
+    #player-ads,
+    .ytd-mealbar-promo-renderer,
+    #watch-channel-brand-div,
+    #watch7-sidebar-ads,
+    ytd-display-ad-renderer,
+    ytd-compact-promoted-item-renderer,
+    .html5-video-player.ad-showing video {
+      display: none !important;
+    }`;
+
+    if (!document.getElementById("yt-extension-style")) {
+      let styleElement = document.createElement("style");
+      styleElement.id = "yt-extension-style",
+      styleElement.appendChild(document.createTextNode(newCssContent)), 
+      headElement.appendChild(styleElement);
+    }
+
+    if (!document.getElementById("yt-ext-info-bar")) {
+        var divEl = document.createElement("div");
+        divEl.id = "yt-ext-info-bar",
+        divEl.innerText = "NonStop Skipping ads...", 
+        divEl.classList.add("yt-ext-hidden"), 
+        bodyElement.appendChild(divEl);
+    }
+
+
+    if (!this.adBlockSelectors) {
+      return;
+    }
+
+    if (!document.getElementById("adBlock-Selectors")) {
+      const cssContent = `${this.adBlockSelectors} { display: none !important; visibility: hidden !important;}`;
+      const styleEl = document.createElement("style");
+      styleEl.id = "adBlock-Selectors",
+      styleEl.textContent = cssContent,
+      headElement.appendChild(styleEl);
+    }
+
+  }
+
+}
+
+
+class SkipVideoAds {
+
+  constructor(isPlayerContainer, 
+    isSkipButtonContainer, 
+    isSkipAdsEnabled, 
+    lessCurrentTime,
+    lessDurationTime, 
+    minusDurationTime,
+    isSpeedAdsEnabled, 
+    videoSpeed) {
+    this.isPlayerContainer = isPlayerContainer;
+    this.isSkipButtonContainer = isSkipButtonContainer;
+    this.lessCurrentTime = lessCurrentTime;
+    this.lessDurationTime = lessDurationTime;
+    this.minusDurationTime = minusDurationTime;
+    this.isSpeedAdsEnabled = isSpeedAdsEnabled ;
+    this.videoSpeed = videoSpeed;
+    this.isSkipAdsEnabled = isSkipAdsEnabled;
+
+  }
+
+  start() {
+    let isPlayerContainer = this.isPlayerContainer,
+    isSkipButtonContainer = this.isSkipButtonContainer,
+    lessCurrentTime = this.lessCurrentTime,
+    lessDurationTime = this.lessDurationTime,
+    minusDurationTime = this.minusDurationTime,
+    isSpeedAdsEnabled = this.isSpeedAdsEnabled,
+    videoSpeed = this.videoSpeed,
+    isSkipAds = this.isSkipAdsEnabled,
+    tigger = 0;
+
+    clearInterval(tigger),
+    tigger = setInterval(() => {
+      let playerContainer = document.querySelector(isPlayerContainer),
+      //skipButton = document.querySelector(isSkipButtonContainer),
+      tigger = document.getElementById("yt-ext-info-bar");
+      //errorScreen = document.querySelector("#error-screen");
+      try{
+        playerContainer ? (
+        tigger && tigger.classList.remove("yt-ext-hidden"),
+        playerContainer.volume = 0, 
+        isSpeedAdsEnabled &&
+        (playerContainer.playbackRate = videoSpeed),
+        isSkipAds &&
+        playerContainer.currentTime > lessCurrentTime && 
+        playerContainer.currentTime < lessCurrentTime * 3 && 
+        playerContainer.currentTime < playerContainer.duration && 
+        playerContainer.duration > lessDurationTime && 
+        (playerContainer.currentTime = playerContainer.duration - minusDurationTime)
+        ) : tigger && tigger.classList.add("yt-ext-hidden")
+      }catch (e) {
+        console.error(e);
+      }
+    }, 550)
+
+  }
+}
+
+
+class Dialog {
+  isUpdatePopupEnabled = "";
+  isRateUsPopupEnabled = "";
+  isMessage = "";
+
+  constructor(updateVersionChrome,
+    isRateUsPopupEnabled, 
+    nextRatingRequest, 
+    isMessage, 
+    isMessageURL) {
+      this.nextRatingRequest = nextRatingRequest;
+      this.updateVersionChrome = updateVersionChrome;
+      this.isRateUsPopupEnabled = isRateUsPopupEnabled;
+      this.isMessage = isMessage;
+      this.isMessageURL = isMessageURL;
+  }
+
+  start() {
+    if (isIframe()) {
+      return;
+    }
+
+    try {
+      setTimeout(() => {
+        if (this.updateVersionChrome > chrome.runtime.getManifest().version) {
+          this.createDialogPopup(`New Version ${this.updateVersionChrome} Available Please Update..😉`, WEBSTORE);
+        }else{
+          if (this.isRateUsPopupEnabled && this.nextRatingRequest) {
+            const webstoreLink = this.createDialogPopup(chrome.i18n.getMessage("rate") , WEBSTORE_REVIEW);
+            webstoreLink.addEventListener("click", () => {
+              chrome.storage.sync.set({
+                nextRatingRequest: false
+              });
+            });
+          }else{
+            if (!this.isMessage=="") {
+              this.createDialogPopup(this.isMessage, WEBSTORE);
+            }
+          }
+        }
+      },2000);
+    }catch(err) {
+      console.log(err.message);
+    }
+
+  }
+
+  appendPopup(dialog) {
+    domReady(() => {
+      document.body.appendChild(dialog);
+    });
+  }
+
+  handlePopupClose(dialog) {
+    document.body.removeChild(dialog);
+  }
+
+  createDialogPopup(text, link) {
+    const handleClose = () => {
+      this.handlePopupClose(dialog);
+    };
+    // Create dialog
+    const dialog = document.createElement("DIV");
+    dialog.classList.add("mytube-dialog");
+
+    // Create closeIcon
+    const closeIcon = document.createElement("A");
+    closeIcon.classList.add("mytube-close-icon");
+    closeIcon.appendChild(document.createTextNode(" "));
+    closeIcon.addEventListener("click", handleClose);
+    dialog.appendChild(closeIcon);
+
+    // Create header
+    const header = document.createElement("DIV");
+    header.appendChild(
+      document.createTextNode(chrome.i18n.getMessage("app_name"))
+    );
+    header.classList.add("mytube-dialog-header");
+    dialog.appendChild(header);
+
+    // Create ShareLink
+    const webstoreLink = document.createElement("A");
+    webstoreLink.classList.add("mytube-webstore-link");
+    webstoreLink.setAttribute("href", link);
+    webstoreLink.setAttribute("target", "_blank");
+    webstoreLink.appendChild(
+      document.createTextNode(text)
+    );
+
+    webstoreLink.addEventListener("click", handleClose);
+    dialog.appendChild(webstoreLink);
+
+    const stylesheet = document.createElement("style");
+    stylesheet.type = "text/css";
+    stylesheet.appendChild(
+      document.createTextNode(`
+      .mytube-dialog {
+        right: 10px;
+        z-index: 99999999999;
+        top: 68px;
+        padding: 0;
+        margin: 0;
+        border-radius: 4px;
+        border: 1px solid white;
+        text-align: center;
+        display: none;
+        background-color: #000000c7;
+        position: fixed;
+      }
+
+      .mytube-close-icon {
+        cursor: pointer;
+        position: absolute;
+        right: 10px;
+        top: 10px;
+        width: 10px;
+        height: 10px;
+        opacity: 0.8;
+      }
+      .mytube-close-icon:hover {
+        opacity: 1;
+      }
+      .mytube-close-icon:before, .mytube-close-icon:after {
+        position: absolute;
+        left: 5px;
+        content: ' ';
+        height: 10px;
+        width: 2px;
+        background-color: white;
+      }
+      .mytube-close-icon:before {
+        transform: rotate(45deg);
+      }
+      .mytube-close-icon:after {
+        transform: rotate(-45deg);
+      }
+
+      .mytube-dialog-header {
+        font-size: 16px;
+        padding: 16px 24px;
+        color: white;
+      }
+
+      .mytube-webstore-link {
+        display: block;
+        font-size: 13px;
+        color: white;
+        padding: 16px 24px;
+        text-decoration: none;
+        opacity: 0.8;
+        border-top: 1px solid white;
+        text-transform: uppercase;
+      }
+
+      .mytube-webstore-link:hover {
+        opacity: 1;
+      }
+    `)
+    );
+    dialog.appendChild(stylesheet);
+    dialog.style.display = "block";
+
+    this.appendPopup(dialog);
+
+    return webstoreLink;
+  }
+
+}
+
+function domReady(callback) {
+  if (document.readyState === "complete") {
+    callback();
+  } else {
+    window.addEventListener("load", callback, {
+      once: true,
+    });
+  }
+}
+
+
+chrome.runtime.sendMessage(
+  {
+    action: "PAGE_READY",
+  },
+  ({adBlockSelectors}) => {
+    const pageUrl = new URL(window.location.href)
+    if (/youtube\.com/.test(window.location.origin)) {
+      info(adBlockSelectors);
+      observer();
+    }
+  }
+);
+
+
+const commentShorts = (comments,shorts) =>{
+  let headElement = document.head || document.getElementsByTagName("head")[0];
+
+  if (!headElement) return void setTimeout((() => {
+   commentShorts(comments,shorts);
+  }), 100);
+ 
+  if (!comments && !document.getElementById("comments-Selectors")) {
+    const cssContent = "ytd-comments { display: none !important; visibility: hidden !important;}";
+    const styleEl = document.createElement("style");
+    styleEl.id = "comments-Selectors",
+    styleEl.textContent = cssContent,
+    headElement.appendChild(styleEl);
+  };
+
+  if (shorts && !document.getElementById("shorts-Selectors")) {
+    const shortsSelectors = [
+      "ytd-rich-shelf-renderer",
+      "ytd-reel-shelf-renderer",
+      ".ytd-reel-shelf-renderer",
+      "a[title=\"Shorts\"]"
+      ];
+
+    this.selectors = shortsSelectors.join(",");
+    const newCssContent = `${this.selectors} { display: none !important; visibility: hidden !important;}`;
+
+    const styleElement = document.createElement("style");
+    styleElement.id = "shorts-Selectors",
+    styleElement.appendChild(document.createTextNode(newCssContent)), 
+    headElement.appendChild(styleElement);
+  };
+
+
+}
+
+const YTnonstop = (nonstop)=>{
+  const observer = new MutationObserver(() => { preventAutostop() });
+
+  // call `observe()`, passing it the element to observe, and the options object
+  if (nonstop) {
+    observer.observe(document.querySelectorAll("ytd-popup-container")[0], {
+      subtree: true,
+      childList: true,
+    });
+  }
+
+}
+
+async function preventAutostop() {
+    console.log("Prevent Autostop: callback that runs when observer is triggered");
+    try{
+      const announcer = document.getElementsByClassName("iron-a11y-announcer");
+      if (announcer.length > 0 && announcer[0].innerText !== "")
+        await removeStillWatchingPopup();
+
+      const reloadRequired = await closeConfirmButton();
+      if (reloadRequired) location.reload();
+      //console.log("Prevent Autostop: if (reloadRequired) location.reload()");
+
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+
+
+async function info(adBlockSelector){
+  var promise = new Promise(function(resolve, reject){
+      chrome.storage.sync.get({
+        "enabled": true,
+        "adBlockingSelectors": adBlockSelector,
+        "isPlayerContainer": ".html5-video-player.ad-showing video",
+        "isSkipButtonContainer": ".ytp-ad-skip-button",
+        "isSkipAdsEnabled": true,
+        "lessCurrentTime": 1.5,
+        "lessDurationTime": 10,
+        "minusDurationTime": 3,
+        "isSpeedAdsEnabled": false,
+        "videoSpeed": 2,
+        "isRecommendEnabled": false,
+        "isRateUsPopupEnabled": true,
+        "updateVersionChrome": chrome.runtime.getManifest().version,
+        "isMessage": "",
+        "isMessageURL": "",
+        "nextRatingRequest": true,
+        "nonstop": true,
+        "comments": true,
+        "shorts": false
+      }, (options)=>{
+        resolve(options)
+      })
+  });
+
+  const options = await promise;
+  commentShorts(options.comments, options.shorts);
+  YTnonstop(options.nonstop);
+  const filters = [
+      new CosmeticFilter(options.adBlockingSelectors),
+      new SkipVideoAds(options.isPlayerContainer, options.isSkipButtonContainer, options.isSkipAdsEnabled, options.lessCurrentTime,options.lessDurationTime, options.minusDurationTime, options.isSpeedAdsEnabled, options.videoSpeed),
+      new Dialog(options.updateVersionChrome, options.isRateUsPopupEnabled,  options.nextRatingRequest, options.isMessage, options.isMessageURL)
+  ];
+   
+  if (options.enabled) {
+    filters.forEach((filter) => {
+      filter.start();
+    });
+  }
+
+
+}
+
+
+
+
+
+
+
+
+   
+          
